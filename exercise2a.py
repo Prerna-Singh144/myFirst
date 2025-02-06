e012fcbbd80b3c8a4128d5b0b98e7048118cc350
Prerna-Singh144 <152497391+Prerna-Singh144@users.noreply.github.com>
2025-02-07 01:05:21 +0530

    Add files via upload

---
+++ exercise2a.py
@@ -0,0 +1,7 @@
+import math
+x1=int(input("enter the x-coordinate of first point:  "))
+y1=int(input("enter the y-coordinate of first point: "))
+x2=int(input("enter the x-coordinate of second point: "))
+y2=int(input("enter the y-coordinate of second point: "))
+distance=math.sqrt((x2-x1)^2+(y2-y1)^2)
+print(distance)
